dd1990f0f7a5618c82cab4ec5ae879de6a1105f3
Liubov Kvasova <bubochka102003@gmail.com>
2021-12-26 23:59:32 +0200

    Added Report table data

---
+++ src/sql/report.sql
@@ -0,0 +1,12 @@
+INSERT INTO Report (id, name, description, DataStream_id)
+VALUES
+        (1, 'Hyouka series', 'The anime series about little girl Hyouka', 1),
+        (2, 'Dog barks at empty bottle', 'A movie containing little dog (chihuahua) which barks at empty wine bottle', 2),
+        (3, 'Tiny cats', 'A movie containing tiny cats which playing with threads and balls', 3),
+        (4, 'Sweet present', 'A film which plot is about sweet present presented to homeless old man', 4),
+        (5, 'Merry Christmass', 'A movie containing Volodymyr Zelensky greeting Ukrainians with Christmass', 5),
+        (6, 'Cute penguin', 'A movie containing little penguin at one station on Antarctica', 6),
+        (7, 'Magestic mask', 'An advertisement of a Gucci mask which costs $500', 7),
+        (8, 'Magic out of Hogwarts', 'A movie containing man who wrote course work in 1 hour', 8),
+        (9, 'I do not know', 'A movie containing women who stands in shop for 6 hours because she does not know what she should buy', 9)
+
